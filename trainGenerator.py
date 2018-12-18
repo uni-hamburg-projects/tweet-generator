ec8863b5888e24ec8966efb931d4954445d56427
ilay köksal <koksalil@itu.edu.tr>
2018-12-18 15:07:50 +0100

    Add files via upload

---
+++ trainGenerator.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 10 15:59:00 2018
+
+@author: ATheb
+"""
+import numpy as np
+from keras.utils import Sequence
+import gensim
+
+  #  embedd training data
+  #   for i in range(0,len(train_X)):
+  #       goal = train_Y[i]
+  #       for t in range(0,len(train_X[i])):
+  #          word = train_X[i][t]
+  #           if word in model.wv.vocab:
+  #               wordVec = model.wv[word]
+  #           else:
+  #               wordVec = np.zeros(100) # if word unknown (e.g. padchar) assign 0 vector
+  #           train_X[i][t] = wordVec
+  #       if goal in model.wv.vocab: 
+  #           train_Y[i] = model.wv[goal]
+  #       else:
+  #           train_Y[i]=np.zeros(100)  
+            
+    
+class DataGenerator(Sequence):
+    def __init__(self, sentence_list, next_word_list, batch_size, maxlen, model):
+        self.batch_size = batch_size
+        self.sentence_list = sentence_list
+        self.next_word_list = next_word_list
+        self.maxlen = maxlen
+        self.model = model
+
+    def __len__(self):
+        return int(len(self.sentence_list)/self.batch_size) + 1
+
+    def __getitem__(self, idx):
+        # batch size depends on number of words per sentence
+        # can use different lengths for batches
+        # inside batch has to have the same length ->sentence packages
+        x = np.empty((self.batch_size, self.maxlen, 100), dtype=np.float64)
+        y = np.empty((self.batch_size, 100), dtype=np.float64)
+        
+        for i in range(idx, idx+self.batch_size):
+            for t in range(0, self.maxlen):
+                word = self.sentence_list[i][t]
+                if word in self.model.wv.vocab:
+                    wordVec = self.model.wv[word]
+                else:
+                    wordVec = np.zeros(100)
+                x[i-idx, t] = wordVec
+            goal = self.next_word_list[i]
+            if goal in self.model.wv.vocab:
+                goalVec = self.model.wv[goal]
+            else:
+                goalVec = np.zeros(100)
+            y[i-idx] = goalVec
+        return x, y
---
+++ tweets.py
@@ -0,0 +1,287 @@
+import gensim
+from keras import Sequential
+from pathlib import Path
+from keras.layers import LSTM, Dense
+from keras.callbacks import ModelCheckpoint
+from keras.callbacks import LambdaCallback
+import numpy as np
+from functools import reduce
+from collections import Counter
+from trainGenerator import DataGenerator
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from sklearn.model_selection import train_test_split
+
+def data_prepare():
+    with open("tweetsTrump.txt", errors="ignore") as f:
+        content = f.readlines()
+        tweets = []
+        for tweet in content:
+            if ((tweet.find("http") == -1) and (tweet.find("https") == -1) and (tweet.find("www")== -1)):
+                tweet = tweet.replace('“',' ').replace('”',' ').replace('. ',' ').replace(',',' ').replace('!',' ').replace('?',' ').replace('.@','@').replace('&amp;','and').replace(': ',' ').replace('– ',' ').replace('- ',' ').replace('— ',' ').replace('--',' ').replace('....','').replace('...','').replace('"','').replace(':…','').replace(':','').replace("'","").replace(" )",")").replace(").",")").replace('.','').replace('#',' #')
+                tweet = gensim.parsing.preprocessing.strip_multiple_whitespaces(tweet)
+                tweet = tweet.strip() #remove initial and lingering white spaces
+                if not tweet.isspace() and tweet:
+                    tweets.append(tweet.lower())
+            else:
+                if tweet.find("http") != -1:
+                    tweet = ' '.join(word for word in tweet.split(' ') if not word.__contains__('http'))
+                if tweet.find("https") != -1:
+                    tweet = ' '.join(word for word in tweet.split(' ') if not word.__contains__('https'))
+                if tweet.find("www") != -1:
+                    tweet = ' '.join(word for word in tweet.split(' ') if not word.__contains__('www'))
+                tweet = tweet.replace('“',' ').replace('”',' ').replace('. ',' ').replace(',',' ').replace('!',' ').replace('?',' ').replace('.@','@').replace('&amp;','and').replace(': ',' ').replace('– ',' ').replace('- ',' ').replace('— ',' ').replace('--',' ').replace('....','').replace('...','').replace('"','').replace(':…','').replace(':','').replace("'","").replace(" )",")").replace(").",")").replace('.','').replace('#',' #')
+                tweet = gensim.parsing.preprocessing.strip_multiple_whitespaces(tweet)
+                tweet = tweet.strip() #remove initial and lingering white spaces
+                if not tweet.isspace() and tweet:
+                    tweets.append(tweet.lower())
+
+    with open('tweets.txt', "w") as file:
+        for t in tweets:
+            file.write(t)
+            file.write("\n")
+
+
+def word2vec_train():
+    documents = []
+    with open("tweets.txt", errors="ignore") as f:
+        content = f.readlines()
+        for tweet in content:
+            # add preprocess stuff
+            tweet = tweet.split()
+            documents.append(tweet)
+
+    for s in documents:
+        s.append('endchar') #add indicator for end of tweet
+    maxlen = 0
+    for i in range(0, len(documents)):
+        if len(documents[i])> maxlen:
+            maxlen = len(documents[i])
+    
+    #Write most common start words to a file
+    start_comb = [None] * len(documents)
+    for i in range(0,len(documents)):
+        if(len(documents[i])>1):
+            start_comb[i] = documents[i][0] + " " + documents[i][1]
+
+    counts = Counter(start_comb)
+    new_list = sorted(start_comb, key=lambda x: -counts[x])
+    unique = reduce(lambda l, x: l + [x] if x not in l else l, new_list, [])
+
+    with open('start_words.txt', "w") as file:
+        for t in unique:
+            if(not t==None):
+                file.write(t)
+                file.write("\n")
+
+    model = gensim.models.Word2Vec(
+        documents,
+        size=100,
+        window=5,
+        min_count=1)
+
+    model.train(documents, total_examples=len(documents), epochs=10)
+    model.save("w2v_trump.model")
+    return maxlen, documents
+
+
+def lstm_model(emdedding_size,maxlen):
+    model = Sequential()
+    model.add(LSTM(units=emdedding_size, return_sequences=True, input_shape=[maxlen, embedding_size]))
+    model.add(LSTM(units=emdedding_size))
+    model.add(Dense(units=embedding_size, activation = 'linear'))
+    model.compile(optimizer='adam', loss='mean_squared_error') #square-error loss
+    return model
+
+
+def sample(model, word_vector, mode):
+    if(mode == 'greedy'):
+        candidate = model.wv.similar_by_vector(word_vector, topn=1)
+        return candidate[0][0]
+    else:
+        #randomly select one of the 5 most similar words
+        candidates = model.wv.similar_by_vector(word_vector, topn=5)
+        selected = np.random.choice(candidates[0], 1,  p=[0.3, 0.25, 0.2, 0.15, 0.1])
+        return selected[0]
+
+def generate_next(model, text, num_generated=10):
+    generatedText = text.lower.split()
+    wordVecs = []
+    for word in text.lower.split():
+        if word in model.wv.vocab:
+            wordVec = model.wv[word]
+        else:
+            wordVec = np.zeros(100)
+        wordVecs.append(wordVec)
+    for i in range(num_generated):
+        prediction = nn_model.predict(x=np.array(wordVecs).reshape(1,len(text),100))  # prediction will be a vector of 100 numbers
+        pred_word = sample(model, prediction, mode='greedy')
+        generatedText.append(pred_word)
+        if (pred_word == 'endchar'):
+            break
+    return ' '.join(vec for vec in generatedText)
+
+
+def on_epoch_end(epoch, _):
+    #do weight checkpoint
+    filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+    ModelCheckpoint(filepath, monitor='loss', save_weights_only=True, mode='min', period=10)
+    
+    #generate text
+    texts = [] 
+    max_seeds = 6
+    cur_seeds = 0
+    with open("start_words.txt", errors="ignore") as f:
+        content = f.readlines()
+        for start_words in content:
+            texts.append(start_words)     
+            cur_seeds += 1
+            if(cur_seeds >= max_seeds):
+                break
+    print('\nGenerating text after epoch: %d' % epoch)
+    with open('epoch_end.txt', "a") as file:
+        file.write('Generating text after epoch: %d' % epoch)
+        file.write("\n")
+        for text in texts:
+            sample = generate_next(model, text)
+            print('%s... -> %s' % (text, sample))
+            file.write('%s... -> %s' % (text, sample))
+            file.write("\n")
+        file.write("\n")
+    
+def display_closestwords_tsnescatterplot(model, word):
+    
+    arr = np.empty((0,100), dtype='f')
+    word_labels = [word]
+
+    # get close words
+    close_words = model.wv.similar_by_word(word)
+    
+    # add the vector for each of the closest words to the array
+    arr = np.append(arr, np.array([model.wv[word]]), axis=0)
+    for wrd_score in close_words:
+        wrd_vector = model.wv[wrd_score[0]]
+        word_labels.append(wrd_score[0])
+        arr = np.append(arr, np.array([wrd_vector]), axis=0)
+        
+    # find tsne coords for 2 dimensions
+    tsne = TSNE(n_components=2, random_state=0)
+    np.set_printoptions(suppress=True)
+    Y = tsne.fit_transform(arr)
+
+    x_coords = Y[:, 0]
+    y_coords = Y[:, 1]
+    # display scatter plot
+    plt.scatter(x_coords, y_coords)
+
+    for label, x, y in zip(word_labels, x_coords, y_coords):
+        plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points')
+    plt.xlim(x_coords.min()+0.05, x_coords.max()+0.05)
+    plt.ylim(y_coords.min()+0.05, y_coords.max()+0.05)
+    plt.show()
+    plt.savefig('tsne_'+str(word)+'.pdf')
+
+def generateTrainAndValidationSet(sentences, maxlen):
+    train_x_file = Path("train_x.txt")
+    train_y_file = Path("train_y.txt")
+    val_x_file = Path("val_x.txt")
+    val_y_file = Path("val_y.txt")
+    
+    train_X = []
+    train_Y = []
+    val_X = []
+    val_Y = []
+    
+    if not (train_x_file.is_file() & train_y_file.is_file() & val_x_file.is_file() & val_y_file.is_file()):
+        data_x = []
+        data_y= []
+        for sentence in sentences:
+            max = len(sentence)-1
+            for i in range(0, max):
+                if i != max-1:
+                    a = np.repeat("padchar", max-1-i).tolist()
+                    b = sentence[0:(i+1)]
+                    a += b
+                    pad = np.repeat("padchar", maxlen - max).tolist()
+                    pad += a
+                    data_x.append(pad)
+                    data_y.append(sentence[i+1])
+                else:
+                    pad = np.repeat("padchar", maxlen - max).tolist()
+                    pad += sentence[0:(i+1)]
+                    data_x.append(pad)
+                    data_y.append(sentence[i+1]) 
+             
+
+        #split into training and validation set
+        train_X,  val_X, train_Y, val_Y = train_test_split(data_x, data_y, test_size=0.10, random_state=42)
+        #write training and validation set to file
+        with open('train_x.txt', "a") as file:
+            for sentence in train_X:
+                file.write(' '.join(word for word in sentence))
+                file.write("\n")
+
+        with open('train_y.txt', "a") as file:
+            for word in train_Y:
+                file.write(word)
+                file.write("\n")
+                
+        with open('val_x.txt', "a") as file:
+            for sentence in val_X:
+                file.write(' '.join(word for word in sentence))
+                file.write("\n")
+
+        with open('val_y.txt', "a") as file:
+            for word in val_Y:
+                file.write(word)
+                file.write("\n")
+    else:
+        with open("train_x.txt", errors="ignore") as f:
+            content = f.readlines()
+            for tweet in content:
+                tweet = tweet.replace("\n", "").split()
+                train_X.append(tweet)
+
+        with open("train_y.txt", errors="ignore") as f:
+            content = f.readlines()
+            for word in content:
+                train_Y.append(word.replace("\n", ""))
+        
+        with open("val_x.txt", errors="ignore") as f:
+            content = f.readlines()
+            for tweet in content:
+                tweet = tweet.replace("\n", "").split()
+                val_X.append(tweet)
+
+        with open("val_y.txt", errors="ignore") as f:
+            content = f.readlines()
+            for word in content:
+                val_Y.append(word.replace("\n", ""))
+    return train_X, train_Y, val_X, val_Y
+            
+if __name__== "__main__":
+    #data_prepare()
+    maxlen, sentences = word2vec_train()
+    model = gensim.models.Word2Vec.load("w2v_trump.model")
+    print(model)
+    display_closestwords_tsnescatterplot(model, "trump")
+    display_closestwords_tsnescatterplot(model, "hillary")
+    pretrained_weights = model.wv.vectors
+    vocab_size, embedding_size = pretrained_weights.shape
+   
+    #set up neural network
+    sentLens = []
+    nn_model = lstm_model(embedding_size,maxlen)
+    print(nn_model.summary())
+    
+    train_X, train_Y, val_X, val_Y = generateTrainAndValidationSet(sentences, maxlen)
+    #NEED TO USE KERAS UTILS SEQUENCE interface for generator
+    BATCH_SIZE = 20
+    training_batch_generator = DataGenerator(train_X, train_Y, BATCH_SIZE, maxlen, model)
+    validation_batch_generator = DataGenerator(val_X, val_Y, BATCH_SIZE, maxlen, model)
+    print("Training on", str(len(train_X)), "examples, validating on", str(len(val_X)),"examples.\n")
+    nn_model.fit_generator(generator=training_batch_generator,
+    epochs=100, verbose=1,
+    callbacks=[LambdaCallback(on_epoch_end=on_epoch_end)],
+    validation_data=validation_batch_generator)
+
